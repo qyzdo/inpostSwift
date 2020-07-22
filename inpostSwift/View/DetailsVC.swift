6f185df6e2a9b2f7e371170355fe840de6ca6b75
Oskar Figiel <qyzdo@outlook.com>
2020-07-23 01:01:55 +0200

    Added template displaying cells in DetailsVC

--- inpostSwift/View/DetailsVC.swift
+++ inpostSwift/View/DetailsVC.swift
@@ -8,22 +8,51 @@
 import UIKit
 
 class DetailsVC: UIViewController {
+    let tableView = UITableView()
+    var safeArea: UILayoutGuide!
+    var parcel:ParcelModel?
+    var trackingDetailsArray = [Tracking_details]()
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .white
+        trackingDetailsArray = (parcel?.tracking_details)!
+        setupUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupUI() {
+        view.backgroundColor = .white
+        safeArea = view.layoutMarginsGuide
+        setupTableView()
     }
-    */
-
+    
+    func setupTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(CustomCell.self, forCellReuseIdentifier: "cell")
+        tableView.backgroundColor = .gray
+        
+    }
 }
+    
+    extension DetailsVC: UITableViewDelegate, UITableViewDataSource {
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return (parcel?.tracking_details!.count)!
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomCell
+            cell.trackingNumberLabel.text = trackingDetailsArray[indexPath.row].datetime
+            cell.statusLabel.text = trackingDetailsArray[indexPath.row].status!.statusRefactor()
+            return cell
+        }
+        
+    }
--- inpostSwift/View/MainVC.swift
+++ inpostSwift/View/MainVC.swift
@@ -10,7 +10,7 @@ import UIKit
 class MainVC: UIViewController {
     let tableView = UITableView()
     var safeArea: UILayoutGuide!
-    var parcelsList = [ParcelModel]()
+    var parcelsArray = [ParcelModel]()
 
     var trackingNumbers = ["687100708024170011003255", "687100218024170137482623"]
     let apiCaller = ApiCaller()
@@ -18,7 +18,7 @@ class MainVC: UIViewController {
     override func viewDidLoad() {
         for i in 0...trackingNumbers.count - 1 {
             apiCaller.getData(trackingNumber: trackingNumbers[i]) {(parcel: ParcelModel) in
-                self.parcelsList.append(parcel)
+                self.parcelsArray.append(parcel)
                 DispatchQueue.main.async {
                     self.tableView.reloadData()
                 }
@@ -53,18 +53,19 @@ class MainVC: UIViewController {
 
 extension MainVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return parcelsList.count
+        return parcelsArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomCell
-        cell.trackingNumberLabel.text = parcelsList[indexPath.row].tracking_number
-        cell.statusLabel.text = parcelsList[indexPath.row].status!.statusRefactor()
+        cell.trackingNumberLabel.text = parcelsArray[indexPath.row].tracking_number
+        cell.statusLabel.text = parcelsArray[indexPath.row].status!.statusRefactor()
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailsVC()
+        vc.parcel = parcelsArray[indexPath.row]
         self.present(vc, animated: true, completion: nil)
     }
     
